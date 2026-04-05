ccb624d77e8aee8aff895f248f7551d0bd1c1935
Joao Correia <joaope@gmail.com>
2026-04-05 22:46:08 +0100

    eslint and few other props

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,9 +4,17 @@ const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: '@cdktn-contrib/cdktn-module-project',
   description: 'Projen project to construct CDKTN (sub)module repository',
+  authorName: 'Joao Correia',
+  authorEmail: 'joaope@users.noreply.github.com',
+  keywords: ['cdktn', 'terraform', 'infrastructure', 'iac', 'projen'],
+  authorOrganization: true,
   packageManager: javascript.NodePackageManager.NPM,
   projenrcTs: true,
-  eslint: false,
+  projenVersion: '0.99.34',
+  eslintOptions: {
+    prettier: true,
+    dirs: ['src', 'test'],
+  },
   jest: false,
   githubOptions: {
     projenCredentials: github.GithubCredentials.fromApp(),
@@ -14,8 +22,6 @@ const project = new typescript.TypeScriptProject({
   npmAccess: javascript.NpmAccess.PUBLIC,
   releaseToNpm: true,
   npmTrustedPublishing: true,
-  minNodeVersion: '22.22.2',
-  prettier: true,
   minMajorVersion: 1,
   repository: 'https://github.com/cdktn-contrib/cdktn-module-project',
   deps: [
