57643a88d1e36e2837111fd60d035fc59329cb6c
Joao Correia <joaope@gmail.com>
2026-04-05 22:57:35 +0100

    lint

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,19 +1,19 @@
-import { github, javascript, typescript } from 'projen';
+import { github, javascript, typescript } from "projen";
 
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: '@cdktn-contrib/cdktn-module-project',
-  description: 'Projen project to construct CDKTN (sub)module repository',
-  authorName: 'Joao Correia',
-  authorEmail: 'joaope@users.noreply.github.com',
-  keywords: ['cdktn', 'terraform', 'infrastructure', 'iac', 'projen'],
+  defaultReleaseBranch: "main",
+  name: "@cdktn-contrib/cdktn-module-project",
+  description: "Projen project to construct CDKTN (sub)module repository",
+  authorName: "Joao Correia",
+  authorEmail: "joaope@users.noreply.github.com",
+  keywords: ["cdktn", "terraform", "infrastructure", "iac", "projen"],
   authorOrganization: true,
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
-  projenVersion: '0.99.34',
+  projenVersion: "0.99.34",
   eslintOptions: {
     prettier: true,
-    dirs: ['src', 'test'],
+    dirs: ["src", "test"],
   },
   jest: false,
   githubOptions: {
@@ -23,11 +23,9 @@ const project = new typescript.TypeScriptProject({
   releaseToNpm: true,
   npmTrustedPublishing: true,
   minMajorVersion: 1,
-  repository: 'https://github.com/cdktn-contrib/cdktn-module-project',
-  deps: [
-    "projen@0.99.34",
-  ],
-  workflowNodeVersion: '24',
+  repository: "https://github.com/cdktn-contrib/cdktn-module-project",
+  deps: ["projen@0.99.34"],
+  workflowNodeVersion: "24",
 });
 
 project.synth();
